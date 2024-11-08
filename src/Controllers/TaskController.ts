17bc8e1a305145caf39332eee6220cdc4242d214
caatwomann69700 <sdebiani7@gmail.com>
2024-11-08 13:23:25 +0100

    Initial commit

--- src/Controllers/TaskController.ts
+++ src/Controllers/TaskController.ts
@@ -6,10 +6,12 @@ import {
   Param,
   Patch,
   Post,
+  Put,
 } from '@nestjs/common';
 import DeleteTask from '../UseCase/DeleteTask/DeleteTask';
 import GetAllTasksUseCase from '../UseCase/GetAllTasks/GetAllTasksUseCase';
 import SaveTaskDto from '../UseCase/SaveTask/SaveTaskDto';
+import SaveTaskUseCase from '../UseCase/SaveTask/SaveTaskUseCase'; 
 import UseCaseFactory from '../UseCase/UseCaseFactory';
 
 @Controller()
@@ -23,12 +25,15 @@ export default class TaskController {
 
   @Post('/tasks')
   async create(@Body() dto: SaveTaskDto) {
-    // @todo YOU MUST FOLLOW THE SAME IMPLEMENTATION AS OTHER ENDPOINTS
+    const result = await (await this.useCaseFactory.create(SaveTaskUseCase)).handle(dto);
+    return { success: true, task: result }; 
   }
+  
+  
 
-  @Patch('/tasks/:id')
-  async update(@Body() dto: SaveTaskDto) {
-    // @todo YOU MUST FOLLOW THE SAME IMPLEMENTATION AS OTHER ENDPOINTS
+  @Put('/tasks/:id')
+  async update(@Param('id') id: string, @Body() dto: SaveTaskDto) {
+    return (await this.useCaseFactory.create(SaveTaskUseCase)).handle({ id: Number(id), ...dto });
   }
 
   @Delete('/tasks/:id')
